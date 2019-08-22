83345b8e90232ff46cbecf03eadbe2f3b56fcddd
tolumide-ng <tolumideshopein@gmail.com>
2019-08-22 18:26:43 +0100

    chore(users): Setup database for authorsHaven
    - Setup followers table
    - Setup Users table
    - Setup Articles table
    
    [Maintains #168061125]

--- app.py
+++ app.py
@@ -16,6 +16,67 @@ db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
 
-# Run Server
+class User (db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    username = db.Column(db.String(50))
+    email = db.Column(db.String(50), unique=True)
+    admin = db.Column(db.Boolean)
+    active = db.Column(db.Boolean)
+
+    def __init__(self, public_id, username, email, admin, active):
+        self.public_id = public_id
+        self.username = username
+        self.email = email
+        self.admin = admin
+        self.active = active
+
+
+# # User schema
+# class UserSchema(ma.Schema):
+#     class Meta:
+#         fields = ('public_id', 'username', 'email', 'admin', 'active')
+
+
+# # Init schema
+# user_schema = UserSchema(strict=True)
+
+
+class Article (db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    title = db.Column(db.String(100))
+    description = db.Column(db.String(200))
+    content = db.Column(db.String(1000))
+    authorId = db.Column(db.Integer)
+
+    def __init__(self, title, description, content, authorId):
+        self.title = title
+        self.description = description
+        self.content = content
+        self.authorId = authorId
+
+# # User schema
+
+
+# class ArticleSchema(ma.Schema):
+#     class Meta:
+#         fields = ('title', 'description', 'content', 'authorId')
+
+
+# # Init schema
+# user_schema = UserSchema(strict=True)
+
+
+class Follows (db.Model):
+    id = db.Column(db.integer, primary_key=True)
+    followedId = db.Column(db.integer)
+    followeeId = db.Column(db.integer)
+
+    def __init__(self, followedId, followeeId):
+        self.followedId = followedId
+        self.followeeId = followeeId
+
+
+    # Run Server
 if __name__ == '__main__':
     app.run(debug=True)
